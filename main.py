bd9209c3f9da26c71888c5395ab16089bb014147
LAnastasiia <lazorenko@ucu.edu.ua>
2019-05-14 22:20:52 +0300

    small-changes

--- main.py
+++ main.py
@@ -1,36 +1,21 @@
-from flask import Flask, request, render_template, send_from_directory
+import os
+import cv2
+from flask import Flask, request, render_template
+from werkzeug.utils import secure_filename
+
+
+from Screte_PRIVATE.screte_database.database import Database
+import Screte_PRIVATE.screte_cryptography.example_from_Oles as eo
+from Screte_PRIVATE.screte_cryptography.image import Image, ImageLoaderAndSaver, diffie_hellman_key, form_secret_key
 
 
 app = Flask(__name__)
-# app.jinja_env.auto_reload = True
-# app.config['TEMPLATES_AUTO_RELOAD'] = True
-# app.secret_key = "super secret key"
+# os.makedirs(os.path.join(app.instance_path, 'image_store'), True)
+app.jinja_env.auto_reload = True
+app.config['TEMPLATES_AUTO_RELOAD'] = True
+app.secret_key = "s_ec%re^t#k*ey**su-pe!r"
 
-# ~~~~~~~~~~~~~~~~~~~~ Example function -- need db communication.
-
-def get_auth_id(user_name, password):
-    return 0
-
-
-def add_user(name, password):
-    return 4
-
-
-def get_contacts(user_id):
-    return [{"id": 234, "name": "Eric"}, {"id": 345, "name": "Rita"}, {"id": 456, "name": "Tony"}]
-
-
-def has_name(username):
-    return False
-
-
-def get_name(user_id):
-    return "Noy"
-
-def get_info(user_id):
-    return {"id": 456, "name": "Tony"}
-# ~~~~~~~~~~~~~~~~~~~~
-
+db = Database()
 
 
 @app.route('/', methods=["GET", "POST"])
@@ -41,46 +26,69 @@ def hello():
 @app.route('/contacts', methods=["GET", "POST"])
 def contacts():
     if request.method == "POST":
-        user_name, password, rep_password = request.form.get("user_name"), \
-                                            request.form.get("password"), \
-                                            request.form.get("repeated_password")
-        # register
+        # return render_template("contacts.html", contacts=["name", "No contacts - it's a new user"])
+
+        rep_password = request.form.get("new_password_repeated")
         if rep_password:
+            user_name, f_name, l_name, password = request.form.get("new_user_name"),\
+                                                  request.form.get("first_name"),\
+                                                  request.form.get("last_name"), \
+                                                  request.form.get("new_password")
             if rep_password == password:
-                if has_name(user_name):
-                    new_u_id = add_user(user_name, password)
-                    return render_template("contacts.html", contacts=get_contacts(new_u_id))
-                else:
+                if not db.get_user_id(user_name):
+                    u_info = {"username": user_name,
+                              "first_name": f_name,
+                              "last_name": l_name,
+                              "password": password}
+                    if db.add_user(u_info):
+                        return render_template("contacts.html", contacts=[], self_name=user_name)
                     return render_template("main.html", log_msg="", reg_msg="Sorry. This name is already taken.")
-            else:
                 return render_template("main.html", log_msg="", reg_msg="You didn't repeat your password correctly.")
-        # log in
+        # # log in
         else:
-            u_id = get_auth_id(user_name, password)
-            if u_id:
-                return render_template("contacts.html", contacts=get_contacts(u_id))
-            else:
-                return render_template("main.html", reg_msg="", log_msg="Wrong username or password. Try again.")
-    else:  # if request.method == "GET"
-        return render_template("main.html", reg_msg="", log_msg="")
+            user_name, password = request.form.get("user_name"), \
+                                  request.form.get("password")
+
+            print(db.check_password(user_name, password))
+            if db.check_password(user_name, password):
+                return render_template("contacts.html", contacts=db.get_contacts(user_name), self_name=user_name)
+            return render_template("main.html", reg_msg="", log_msg="Wrong username or password. Try again.")
+    return render_template("main.html", reg_msg="", log_msg="")  # if request.method == "GET"
 
 
-@app.route('/send/<int:from_id>/<int:to_id>', methods=["GET", "POST"])
-def send(from_id, to_id):
-    # access to db --> info about both users
-    return render_template("send.html", users=[get_info(from_id), get_info(to_id)])
+@app.route('/send/<string:from_name>/<string:to_name>', methods=["GET", "POST"])
+def send(from_name, to_name):
+    return render_template("send.html", from_name=from_name, to_name=to_name)
 
 
-@app.route('/result/<int:from_id>/<int:to_id>', methods=["GET", "POST"])
-def result(from_id, to_id):
+@app.route('/result/<string:from_name>/<string:to_name>', methods=["GET", "POST"])
+def result(from_name, to_name):
     if request.method == "POST":
-        img = request.form.get("img")
-        if img:
-            # process, save, give url
-            res_url = "https://instagram.fiev21-2.fna.fbcdn.net/vp/ab053c540461f0765c71c72a2d615125/5D666C46/t51.2885-19/s150x150/51939312_838258313180447_8972914188674400256_n.jpg?_nc_ht=instagram.fiev21-2.fna.fbcdn.net"
-            return render_template("result.html", img_url=res_url)
+        # img = request.form.get("img")
+        img = request.files["img"]
+        print(img)
+        if img is not None:
+
+            # save image
+            img_path = os.path.join(app.instance_path, secure_filename(img.filename))
+            img.save(img_path)  # save into "instance" directory
+
+            user_info = diffie_hellman_key()
+
+            # read
+            img = ImageLoaderAndSaver.load_image_locally(img_path)
+            secret_key = form_secret_key(img, user_info)
+            our_img = Image.encrypt_img(img, secret_key)
+            ImageLoaderAndSaver.save_image_locally(our_img, "secret.bmp")
+
+            enc_img = ImageLoaderAndSaver.load_image_locally("secret.bmp")
+            our_img = Image.decrypt_img(enc_img, secret_key)
+            ImageLoaderAndSaver.save_image_locally(our_img, "decrypted.jpg")
+
+        return render_template("result.html", img_url="")
     else:
         return render_template("result.html", img_url="")
+
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True, use_reloader=True)
