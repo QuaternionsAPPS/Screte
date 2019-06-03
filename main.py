6280e7afc17a1b5b20a78a64a4d1cec45b25e3a3
LAnastasiia <lazorenko@ucu.edu.ua>
2019-06-04 01:27:00 +0300

    make it better

--- main.py
+++ main.py
@@ -120,6 +120,7 @@ def result(from_name, to_name):
 
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run()
+    # port = int(os.environ.get('PORT', 5000))
+    # app.run(host='0.0.0.0', port=port)
     # app.run(host='0.0.0.0', port=8080, debug=True)
